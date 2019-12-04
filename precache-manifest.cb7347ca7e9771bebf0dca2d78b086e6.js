1b241a13a6ff51c9b539570133f21dfd190c1a96
JalalMadhaji <jalal.almadhaji90@gmail.com>
2019-12-04 21:28:56 +0300

    Updates

---
+++ precache-manifest.cb7347ca7e9771bebf0dca2d78b086e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68678dcce6ba411067b6c7b70f109b1",
+    "url": "/react-add-delete-assignment/index.html"
+  },
+  {
+    "revision": "a17945f0b5421ba9e1cd",
+    "url": "/react-add-delete-assignment/static/css/main.0146d093.chunk.css"
+  },
+  {
+    "revision": "57911d08d5cfd492b217",
+    "url": "/react-add-delete-assignment/static/js/2.d6e93681.chunk.js"
+  },
+  {
+    "revision": "a17945f0b5421ba9e1cd",
+    "url": "/react-add-delete-assignment/static/js/main.484ab430.chunk.js"
+  },
+  {
+    "revision": "e73f000f10fb2c38c90c",
+    "url": "/react-add-delete-assignment/static/js/runtime-main.018335cd.js"
+  }
+]);
